2b9fd0bcb9ea225475225840122c428fddfe6e7a
Parker Dinsmore <parkermdinsmore@gmail.com>
2018-07-17 21:43:47 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array.join("")
+  elsif array.length == 2
+    return array.join(" and ")
+  elsif array.length > 2
+    final_array_element = array.pop
+    array_string = array.join(", ")
+    return array_string << (", and " + "#{final_array_element}")
+  end
 end
